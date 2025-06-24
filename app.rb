b6879c03bcabede96e95c53dd7c177841566f163
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-06-24 14:22:13 +0000

    commit_taking a lunch break

--- app.rb
+++ app.rb
@@ -25,9 +25,33 @@ get("/rock") do
 end
 
 get("/paper") do
+    moves = ["rock", "paper", "sissors"]
+
+  @comp_move = moves.sample
+  
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
   erb(:giraffe)
 end
 
 get("/sissors") do
+    moves = ["rock", "paper", "sissors"]
+
+  @comp_move = moves.sample
+  
+  if @comp_move == "sissors"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "lost"
+  end
+
   erb(:bear)
 end
